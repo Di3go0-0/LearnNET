3e76ce89599d8370273c6ebcca9a95fbfeab7a4a
di3go0-0 <d.rincon@utp.edu.co>
2024-08-23 20:17:59 -0500

    Configure connectionString database

--- LearnNet/Program.cs
+++ LearnNet/Program.cs
@@ -1,9 +1,17 @@
+using api.data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = builder.Configuration.GetConnectionString("Connection")
+    ?? throw new InvalidOperationException("Connection string is missing");
+
+builder.Services.AddDbContext<AplicationDBContext>(options => options.UseMySQL(connectionString));
 
 var app = builder.Build();
 
