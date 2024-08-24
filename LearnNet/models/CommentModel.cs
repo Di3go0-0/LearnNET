1fae7eb4caee476fa35d943af38e79a6559aa89e
di3go0-0 <d.rincon@utp.edu.co>
2024-08-23 20:15:59 -0500

    Create Models

--- LearnNet/models/CommentModel.cs
+++ LearnNet/models/CommentModel.cs
@@ -2,13 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using api.models;
 
 namespace api.models
 {
     public class Comment
     {
+        public int Id {get; set;}
+        public string Title { get; set; } = string.Empty;
+        public string Content {get; set;} = string.Empty;
+        public DateTime CreatedOn {get; set;} = DateTime.Now;
         public int? StockId { get; set; }
         // Navigation property
         public Stock? Stock { get; set; }
     }
-}
+} 
--- LearnNet/models/StokModel.cs
+++ LearnNet/models/StokModel.cs
@@ -2,21 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
 
 namespace api.models
 {
-    public class Stok
+    public class Stock
     {
         public int Id { get; set; }
-        public string Symbol { get; set; } = string.empty;
-        public string CompanyName { get; set; } = string.empty;
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Purchase { get; set; }
         [Column(TypeName = "decimal(18, 2)")]
         public decimal LastDiv { get; set; }
-        public string Industry { get; set; } = string.empty;
+        public string Industry { get; set; } = string.Empty;
         public long MyProperty { get; set; }
 
-        public List<Comment> Comments { get; set; } = new list<Comment> ();
+        public List<Comment> Comments { get; set; } = new List<Comment> ();
     }
 }
