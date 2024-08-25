9168e7a6541b91938e0cccd74ec46aef78339eb4
di3go0-0 <d.rincon@utp.edu.co>
2024-08-24 19:15:04 -0500

    Create Stocks Controllers {GetAll, GetById}

---
+++ LearnNet/controllers/StackController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using api.models;
+using api.data;
+
+namespace api.controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StackController : ControllerBase 
+    {
+        private readonly AplicationDBContext _context;
+        public StackController(AplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll(){
+            var stocks = _context.Stocks.ToList();
+            return Ok(new { message = "Stocks Found", data = stocks });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id){
+            var stock = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if(stock == null){
+                return NotFound(new { message = "Stock not found" });
+            }
+            return Ok(new { message = "Stock Found", data = stock });
+        }
+    }
+}
