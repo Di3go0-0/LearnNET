362d422ff9cc49e787d00d08f36698d3eea950d4
di3go0-0 <d.rincon@utp.edu.co>
2024-08-24 21:30:59 -0500

    Update Stock Controller

--- LearnNet/Controllers/StackController.cs
+++ LearnNet/Controllers/StackController.cs
@@ -7,33 +7,69 @@ using Microsoft.EntityFrameworkCore;
 using api.models;
 using api.data;
 using api.Mappers;
+using api.DTOs.Stock;
 
 namespace api.controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class StackController : ControllerBase 
+    public class StockController : ControllerBase
     {
         private readonly AplicationDBContext _context;
-        public StackController(AplicationDBContext context)
+        public StockController(AplicationDBContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public IActionResult GetAll(){
+        public IActionResult GetAll()
+        {
             var stocks = _context.Stocks.ToList()
-            .Select(s =>s.ToStockDto());
+            .Select(s => s.ToStockDto());
             return Ok(new { message = "Stocks Found", data = stocks });
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById(int id){
+        public IActionResult GetById(int id)
+        {
             var stock = _context.Stocks.FirstOrDefault(x => x.Id == id);
-            if(stock == null){
+            if (stock == null)
+            {
                 return NotFound(new { message = "Stock not found" });
             }
             return Ok(new { message = "Stock Found", data = stock.ToStockDto() });
         }
+
+        [HttpPost]
+        public IActionResult CreateStock([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDTO();
+            _context.Stocks.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
+
+
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateStock([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound(new { message = "Stock not found" });
+            }
+            stockModel.Symbol = updateDto.Symbol;
+            stockModel.CompanyName = updateDto.CompanyName;
+            stockModel.Purchase = updateDto.Purchase;
+            stockModel.LastDiv = updateDto.LastDiv;
+            stockModel.Industry = updateDto.Industry;
+            stockModel.MarketCap = updateDto.MarketCap;
+
+            _context.SaveChanges();
+            return Ok(new { message = "Stock Updated", data = stockModel.ToStockDto() });
+        }
+
     }
 }
