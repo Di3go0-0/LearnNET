b7f000dc0d21d5b5d07cbb0bc28b8d4c49775c42
di3go0-0 <d.rincon@utp.edu.co>
2024-08-25 09:00:30 -0500

    Modified program.cs to use Stock Repository

--- LearnNet/Program.cs
+++ LearnNet/Program.cs
@@ -1,4 +1,6 @@
 using api.data;
+using api.Interfaces;
+using api.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +15,8 @@ var connectionString = builder.Configuration.GetConnectionString("Connection")
     ?? throw new InvalidOperationException("Connection string is missing");
 
 builder.Services.AddDbContext<AplicationDBContext>(options => options.UseMySQL(connectionString));
+
+builder.Services.AddScoped<IStockRepository, StockRepository>();
 
 var app = builder.Build();
 
