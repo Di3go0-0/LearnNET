a889a8d075f021778af9ec980702c63099f28575
di3go0-0 <d.rincon@utp.edu.co>
2024-08-25 20:48:33 -0500

    Create a necessary Comment's Dto's

---
+++ LearnNet/DTOs/Comment/CreateCommentRequestDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.DTOs.Comment
+{
+    public class CreateCommentRequestDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Content {get; set;} = string.Empty;
+        public DateTime CreatedOn {get; set;} = DateTime.Now;
+        public int? StockId { get; set; }
+    }
+}
