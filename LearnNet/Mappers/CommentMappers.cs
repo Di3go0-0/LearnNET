54dc8eb4a18a69c539ed075515eafc374e2994a5
di3go0-0 <d.rincon@utp.edu.co>
2024-08-25 20:50:03 -0500

    Create a Comment Mappers

---
+++ LearnNet/Mappers/CommentMappers.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.DTOs.Comment;
+using api.models;
+
+namespace api.Mappers
+{
+    public static class CommentMappers
+    {
+        public static CommentDto ToCommentDto(this Comment commentModel)
+        {
+            return new CommentDto
+            {
+                Id = commentModel.Id,
+                Title = commentModel.Title,
+                Content = commentModel.Content,
+                CreatedOn = commentModel.CreatedOn,
+                StockId = commentModel.StockId
+            };
+        }
+
+        public static Comment ToCommentFromCreateDTO(this CreateCommentRequestDto commentDto)
+        {
+            return new Comment
+            {
+                Title = commentDto.Title,
+                Content = commentDto.Content,
+                CreatedOn = commentDto.CreatedOn,
+                StockId = commentDto.StockId
+            };
+        }
+    }
+}
