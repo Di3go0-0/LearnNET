959e4a4d6291e25590d937c007d9c28c8daba2a1
di3go0-0 <d.rincon@utp.edu.co>
2024-08-26 20:16:48 -0500

    Modified Update Comment Dto to data valitadion

--- LearnNet/DTOs/Comment/UpdateCommentRequestDto.cs
+++ LearnNet/DTOs/Comment/UpdateCommentRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,13 @@ namespace api.DTOs.Comment
 {
     public class UpdateCommentRequestDto
     {
+        [Required]
+        [MinLength(5, ErrorMessage = "Title must be at least 5 characters long")]
+        [MaxLength(50, ErrorMessage = "Title must be at most 50 characters long")]
         public string Title { get; set; } = string.Empty;
+        [Required]
+        [MinLength(5, ErrorMessage = "Content must be at least 5 characters long")]
+        [MaxLength(500, ErrorMessage = "Content must be at most 500 characters long")]
         public string Content {get; set;} = string.Empty;
     }
     
