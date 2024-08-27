73a79d8cf5bb040f842de20ee344cef3637ce6c2
di3go0-0 <d.rincon@utp.edu.co>
2024-08-26 20:21:50 -0500

    Modified Stack controller to verify the data validation

--- LearnNet/Controllers/StackController.cs
+++ LearnNet/Controllers/StackController.cs
@@ -25,7 +25,11 @@ namespace api.controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var stocks =  await _stockRepository.GetAllStocksAsync();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var stocks = await _stockRepository.GetAllStocksAsync();
             var stockDto = stocks.Select(s => s.ToStockDto());
             return Ok(new { message = "Stocks Found", data = stockDto });
         }
@@ -34,6 +38,10 @@ namespace api.controllers
         [Route("{id:int}")]
         public async Task<IActionResult> GetById([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             var stock = await _stockRepository.GetStockAsync(id);
             if (stock == null)
             {
@@ -45,6 +53,10 @@ namespace api.controllers
         [HttpPost]
         public async Task<IActionResult> CreateStock([FromBody] CreateStockRequestDto stockDto)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             var stockModel = await _stockRepository.CreateStockAsync(stockDto);
 
             return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
@@ -55,13 +67,17 @@ namespace api.controllers
         [Route("{id:int}")]
         public async Task<IActionResult> UpdateStock([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             var stockModel = await _stockRepository.UpdateStockAsync(id, updateDto);
 
             if (stockModel == null)
             {
                 return NotFound(new { message = "Stock not found" });
             }
-            
+
             return Ok(new { message = "Stock Updated", data = stockModel.ToStockDto() });
         }
 
@@ -74,7 +90,7 @@ namespace api.controllers
             {
                 return NotFound(new { message = "Stock not found" });
             }
-            return Ok(new { message = "Stock Deleted" , data = stockModel.ToStockDto() });
+            return Ok(new { message = "Stock Deleted", data = stockModel.ToStockDto() });
         }
 
     }
