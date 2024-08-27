4f1a703d5ec6a7933942d57e3077bece0c23f5e8
di3go0-0 <d.rincon@utp.edu.co>
2024-08-26 20:37:34 -0500

    Modified IStock repository to search through query

--- LearnNet/Interfaces/IStockRepository.cs
+++ LearnNet/Interfaces/IStockRepository.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.DTOs.Stock;
+using api.Helpers;
 using api.models;
 
 namespace api.Interfaces
 {
     public interface IStockRepository
     {
-        Task<List<Stock>> GetAllStocksAsync();
+        Task<List<Stock>> GetAllStocksAsync(QueryObject query);
         Task<Stock?> GetStockAsync(int id);
         Task<Stock> CreateStockAsync(CreateStockRequestDto stock);
         Task<Stock?> UpdateStockAsync(int id, UpdateStockRequestDto stock);
