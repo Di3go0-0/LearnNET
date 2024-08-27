cc6e8bd12b672e80760cff10a440e674c339312a
di3go0-0 <d.rincon@utp.edu.co>
2024-08-26 20:17:23 -0500

    Modified Update Stock Dto to data valitadion

--- LearnNet/DTOs/Stock/UpdateStockRequestDto.cs
+++ LearnNet/DTOs/Stock/UpdateStockRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,23 @@ namespace api.DTOs.Stock
 {
     public class UpdateStockRequestDto
     {
+        [Required]
+        [MaxLength(10, ErrorMessage = "Symbol must be at most 10 characters long")]
         public string Symbol { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(50, ErrorMessage = "Company name must be at most 50 characters long")]
         public string CompanyName { get; set; } = string.Empty;
+        [Required]
+        [Range(1,1000, ErrorMessage = "Purchase price must be between 1 and 1000")]
         public decimal Purchase { get; set; }
+        [Required]
+        [Range(0.001, 100, ErrorMessage = "Last dividend must be between 0.001 and 100")]
         public decimal LastDiv { get; set; }
+        [Required] 
+        [MaxLength(10, ErrorMessage = "Industry must be at most 10 characters long")]
         public string Industry { get; set; } = string.Empty;
+        [Required]
+        [Range(1,5000000000, ErrorMessage = "Market cap must be between 1 and 5000000000")]
         public long MarketCap { get; set; }
     }
 }
