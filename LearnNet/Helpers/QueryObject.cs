87a69c4f69a515f345180015b6146faaa79289e9
di3go0-0 <d.rincon@utp.edu.co>
2024-08-28 20:19:11 -0500

    Implement Paginarion

--- LearnNet/Helpers/QueryObject.cs
+++ LearnNet/Helpers/QueryObject.cs
@@ -10,6 +10,8 @@ namespace api.Helpers
         public string? Symbol { get; set; } = null;
         public string? CompanyName { get; set; } = null;
         public string? SortBy { get; set; } = null;
-        public bool IsDecsending { get; set; } = false; 
+        public bool IsDecsending { get; set; } = false;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20; 
     }
 }
