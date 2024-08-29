02605a36c3399338db4749fd59db5204d679587b
di3go0-0 <d.rincon@utp.edu.co>
2024-08-28 20:19:28 -0500

    Implement Paginarion

--- LearnNet/Repository/StockRepository.cs
+++ LearnNet/Repository/StockRepository.cs
@@ -44,8 +44,9 @@ namespace api.Repository
                             stocks.OrderBy(s => s.Symbol);
                 }
             }
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
 
-            return await stocks.ToListAsync();
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetStockAsync(int id)
