634b7d8746337751be74ebc708020f3a4069b625
di3go0-0 <d.rincon@utp.edu.co>
2024-08-28 20:12:50 -0500

    Modified QueryObject to implement sorting

--- LearnNet/Helpers/QueryObject.cs
+++ LearnNet/Helpers/QueryObject.cs
@@ -7,7 +7,9 @@ namespace api.Helpers
 {
     public class QueryObject
     {
-        public string? Symbol { get; set; }
-        public string? CompanyName { get; set; }
+        public string? Symbol { get; set; } = null;
+        public string? CompanyName { get; set; } = null;
+        public string? SortBy { get; set; } = null;
+        public bool IsDecsending { get; set; } = false; 
     }
 }
