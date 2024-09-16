7bce77febf99a563e168e4ed6bafe2526841537e
di3go0-0 <d.rincon@utp.edu.co>
2024-09-15 21:36:04 -0500

    Others

---
+++ LearnNet/Migrations/20240916021838_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "29595781-13e0-4baa-b58c-c24632a279ea", null, "Admin", "ADMIN" },
+                    { "6a48e0be-cdab-4e40-b70d-4cf17a86ec08", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "29595781-13e0-4baa-b58c-c24632a279ea");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6a48e0be-cdab-4e40-b70d-4cf17a86ec08");
+        }
+    }
+}
