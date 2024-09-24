8c9cc4681629c8c491832e242061259b415c19d2
di3go0-0 <d.rincon@utp.edu.co>
2024-09-23 20:48:51 -0500

    Add Login endpoint

--- LearnNet/Controllers/AccountController.cs
+++ LearnNet/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using api.DTOs.Account;
 using api.Interfaces;
+using Microsoft.EntityFrameworkCore;
 namespace api.Controllers
 {
     [Route("api/account")]
@@ -15,10 +16,38 @@ namespace api.Controllers
     {
         private readonly UserManager<UserModel> _userManager;
         private readonly ITokenService _tokenService;
-        public AccountController(UserManager<UserModel> userManager, ITokenService tokenService)
+        private readonly SignInManager<UserModel> _signInManager;
+        public AccountController(UserManager<UserModel> userManager, ITokenService tokenService, SignInManager<UserModel> signInManager)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login( [FromBody] LoginDto loginDto)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest("Invalid data");
+            
+            var user = await _userManager.Users.FirstOrDefaultAsync (x => x.UserName == loginDto.Username);
+
+            if (user == null)
+                return Unauthorized("Invalid username");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if(!result.Succeeded)
+                return Unauthorized("Invalid password");
+
+            return Ok(
+                new{
+                UserName = user.UserName,
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user)
+                }
+            );
+
         }
 
         [HttpPost("register")]
@@ -38,6 +67,15 @@ namespace api.Controllers
                 var existingEmail = await _userManager.FindByEmailAsync(registerDto.Email);   
                             
                 if (existingEmail != null)
+                    return BadRequest("User already exists");
+                
+                if (string.IsNullOrEmpty(registerDto.Username))
+                {
+                    return BadRequest("Username is required");
+                }
+
+                var existingUser = await _userManager.FindByNameAsync(registerDto.Username);
+                if (existingUser != null)
                     return BadRequest("User already exists");
 
                 var user = new UserModel
