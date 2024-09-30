1153dd0b1c57e943874640c44c190744d003f2e4
di3go0-0 <d.rincon@utp.edu.co>
2024-09-29 21:38:38 -0500

    Create a Many-To-Many

--- LearnNet/Data/AplicationDBContext.cs
+++ LearnNet/Data/AplicationDBContext.cs
@@ -14,9 +14,22 @@ namespace api.data
     }
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<Portfolio>(x => x.HasKey(y => new { y.AppUserId, y.StockId }));
+
+            builder.Entity<Portfolio>()
+                .HasOne(x => x.AppUser)
+                .WithMany(x => x.Portfolios)
+                .HasForeignKey(x => x.AppUserId);
+
+            builder.Entity<Portfolio>()
+                .HasOne(x => x.Stock)
+                .WithMany(x => x.Portfolios)
+                .HasForeignKey(x => x.StockId);
 
             List<IdentityRole> roles = new List<IdentityRole>
             {
